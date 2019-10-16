d169a085d96c76ee18a3dcd1f44b8e476b6ab888
Larinayc <56049823+Larinayc@users.noreply.github.com>
2019-10-16 14:39:56 -0700

    Add files via upload

---
+++ Rock Paper Scissors/RockPaperScissors.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class RockPaperScissors{
+
+    public static void main (String [] args){
+        int wins = 0;
+        int ties = 0;
+        int losses = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Let's play rock, paper, scissors, human! Goodluck.");
+        System.out.println("");
+
+        String again = "y";
+
+        while (again.equals("y")){
+            int comp = (int) (Math.random()* 3) + 1;
+
+            System.out.println("What do you throw? [1] Rock, [2] Paper, [3] Scissor?");
+            int human = sc.nextInt();
+            if ( human != 1 || human != 2 || human != 3 )
+                System.out.println ("Invalid response, silly human!");
+            
+            if (human == 1 ){
+                if (comp == 2) {
+                    System.out.println("You threw rock! I threw paper!");
+                    System.out.println("I win! You are not a challenge for me ...");
+                    losses ++;
+                }
+                else if (comp == 3) {
+                    System.out.println("You threw rock! I threw scissor!");
+                    System.out.println("Whaaa??... You beat me? RAGE!");
+                    wins ++;
+                }
+                else{
+                    System.out.println ("A draw ... pretty good, human! I demand a rematch ...");
+                    ties ++;
+                }
+            }
+            if (human == 2 ){
+                if (comp == 3) {
+                    System.out.println("You threw paper! I threw scissor!");
+                    System.out.println("I win! You are not a challenge for me ...");
+                    losses ++;
+                }
+                else if (comp == 1) {
+                    System.out.println("You threw paper! I threw rock!");
+                    System.out.println("Whaaa??... You beat me? RAGE!");
+                    wins ++;
+                }
+                else{
+                    System.out.println ("A draw ... pretty good, human! I demand a rematch ...");
+                    ties ++;
+                }
+            }
+            if (human == 3 ){
+                if (comp == 1) {
+                    System.out.println("You threw scissors! I threw rock!");
+                    System.out.println("I win! You are not a challenge for me ...");
+                    losses ++;
+                }
+                else if (comp == 2) {
+                    System.out.println("You threw scissors! I threw paper!");
+                    System.out.println("Whaaa??... You beat me? RAGE!");
+                    wins ++;
+                }
+                else{
+                    System.out.println ("A draw ... pretty good, human! I demand a rematch ...");
+                    ties ++;
+                }
+            }
+
+            System.out.println("");
+            System.out.println ("Play again? [y,n]");
+            again = sc.next();
+            System.out.println("");
+
+        }
+
+        System.out.println ("Good Game! Thanks for playing.");
+        System.out.println("");
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Ties: " + ties);
+    }
+}
